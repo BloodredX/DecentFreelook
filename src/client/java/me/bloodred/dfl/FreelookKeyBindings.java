ee1c02878e78d92c7481f7f87c607bef6f96d2e3
BloodredX <aadinathg2000@gmail.com>
2026-03-04 22:04:30 +0530

    Complete update to Minecraft 26.1-snapshot-11 (Deobfuscated mode)

--- src/client/java/me/bloodred/dfl/FreelookKeyBindings.java
+++ src/client/java/me/bloodred/dfl/FreelookKeyBindings.java
@@ -1,6 +1,6 @@
 package me.bloodred.dfl;
 
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
 import com.mojang.blaze3d.platform.InputConstants;
@@ -14,7 +14,7 @@ public class FreelookKeyBindings {
     public static KeyMapping FREELOOK_KEY;
 
     public static void register() {
-        FREELOOK_KEY = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        FREELOOK_KEY = KeyMappingHelper.registerKeyMapping(new KeyMapping(
             "key.decentfreelook.freelook",
             InputConstants.Type.KEYSYM,
             GLFW.GLFW_KEY_F6,
